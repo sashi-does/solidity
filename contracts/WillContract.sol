39377565d2ad975d4f9235c39fbdfc96b1b463e4
sashi-does <sashiontop@gmail.com>
2025-08-11 01:03:50 +0530

    added will contract

---
+++ contracts/WillContract.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract WillContract {
+    address private owner;
+    address private recipient;
+    uint256 private latestPing;
+    uint private tenYears = 10 * 365 days;
+
+    constructor(address payable _owner) {
+        owner = _owner;
+        latestPing = block.timestamp;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only Admin can access");
+        _;
+    }
+
+    modifier onlyRecipient() {
+        require(msg.sender == recipient, "Only my family can access");
+        _;
+    }
+
+    function setRecipient(address _recipient) public onlyOwner {
+        require(msg.sender == owner, "Only Admin can set/modify the recipient");
+        recipient = _recipient;
+    }
+
+    function ping() public {
+        latestPing = block.timestamp;
+    }
+
+    function addFunds() public onlyOwner payable  {
+        //-> ETH is automatically accepted since it is marked as payable
+        // payable(address(this)).transfer(msg.value);
+    }
+
+    function drain() public onlyRecipient  {
+        require(block.timestamp - latestPing >= tenYears , "Shouldn't be calling for 10 years");
+        payable(recipient).transfer(address(this).balance);
+    }
+    
+}
