471d306c16b77bec5beea30a3ede51e96da7dc12
Sashi Warddhan <sashiontop@gmail.com>
2025-09-08 17:45:07 +0530

    Update CCI.sol

--- remix/CCI.sol
+++ remix/CCI.sol
@@ -1,1 +1,30 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// address: 0x1d142a62E2e98474093545D4A3A0f7DB9503B8BD
+contract A {
+    int public a;
+
+    constructor() { 
+        a = 2;
+    }
+
+    function double() public {
+        a = a * 2;
+    }
+}
+
+interface IA {
+    function double() external;
+}
+
+contract B {
+    
+    address addressA = 0x1d142a62E2e98474093545D4A3A0f7DB9503B8BD;
+    
+    function modifyA() public {
+        IA(addressA).double();
+    }
+
+}
